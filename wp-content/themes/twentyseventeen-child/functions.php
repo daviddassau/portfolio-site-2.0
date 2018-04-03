feb61cb8dc51e1c16133603dc1cac8f8fbf26ebf
David Dassau <todieahero@hotmail.com>
2018-04-03 17:12:41 -0500

    added new styles and other stuff

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -33,4 +33,12 @@ function theme_js() {
 
 add_action( 'wp_enqueue_scripts', 'theme_js');
 
+add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
+function enqueue_load_fa() {
+ 
+    wp_enqueue_style( 'load-fa', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+ 
+}
+
+
 ?>
